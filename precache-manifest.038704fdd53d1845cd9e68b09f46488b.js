d5b587eccc36eaa662e397f1d0f2743c690922cd
Asarael Navarro <asarael.navarro@gmail.com>
2022-05-19 12:23:40 -0500

    Updates

---
+++ precache-manifest.038704fdd53d1845cd9e68b09f46488b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed53128441da1fbf4380",
+    "url": "/mycv/static/css/main.85b169f5.chunk.css"
+  },
+  {
+    "revision": "ed53128441da1fbf4380",
+    "url": "/mycv/static/js/main.ed531284.chunk.js"
+  },
+  {
+    "revision": "b85594065d3f13e83130",
+    "url": "/mycv/static/js/1.b8559406.chunk.js"
+  },
+  {
+    "revision": "a535af63239fb12c81dc",
+    "url": "/mycv/static/js/runtime~main.a535af63.js"
+  },
+  {
+    "revision": "7d2350aa19c7812ae60b281d4cc403a2",
+    "url": "/mycv/static/media/perfil.7d2350aa.jpg"
+  },
+  {
+    "revision": "b368aff5e8e3a60a36e6b11dd8dad61b",
+    "url": "/mycv/static/media/logo.b368aff5.svg"
+  },
+  {
+    "revision": "843eeb2cb512324dc857fc78eaadf2b2",
+    "url": "/mycv/index.html"
+  }
+];
